416416e4f8d424159418457928923380215cd5ba
Sanm-ZH <zhxpvip7@gmail.com>
2021-05-12 16:15:01 +0800

    Update 更新脚本

--- push-remote.sh
+++ push-remote.sh
@@ -3,11 +3,13 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-git push origin master:master
+branch=$(git symbolic-ref --short -q HEAD)
+
+git push origin $branch:$branch
 echo -e '\033[32m 已推送至sanm-zh.github.io \n'
 
 git remote set-url origin git@gitee.com:sanm-zh/resume.git
-git push origin master:master
+git push origin $branch:$branch
 echo -e '\033[32m 已推送至sanm-zh.gitee.io \n'
 
 git remote set-url origin git@github.com:Sanm-ZH/resume.git
